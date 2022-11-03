41ed6b111e313e2a0fbe7b8df7039265047f989d
임재현 <ljh040513@naver.com>
2022-11-03 13:17:57 +0900

    [ADD] Login xml add
    ADD - 로그인 페이지 xml 추가

--- MiWorkDas/app/src/main/java/com/midas/miworkdas/viewmodel/LoginViewModel.kt
+++ MiWorkDas/app/src/main/java/com/midas/miworkdas/viewmodel/LoginViewModel.kt
@@ -1,6 +1,14 @@
 package com.midas.miworkdas.viewmodel
 
+import android.os.Build.VERSION_CODES.M
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import com.midas.miworkdas.Base.BaseViewModel
 
 class LoginViewModel : BaseViewModel() {
+    val inputCode = MutableLiveData<String>()
+
+    fun onClickLoginBtn(){
+        //로그인 처리
+    }
 }
